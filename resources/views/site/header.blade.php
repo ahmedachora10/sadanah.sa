389b2c24b129475c085094cf17544b6e91506540
ahmedachora10 <gjr8195@gmail.com>
2024-12-11 02:18:32 +0100

    add direction & lang attributes to html page

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -2,7 +2,7 @@
     $lang = app()->getLocale();
 @endphp
 <!DOCTYPE html>
-<html lang="{{$lang}}" dir="{{$lang == 'ar' : 'rtl' : 'ltr'}}">
+<html lang="{{$lang}}" dir="{{$lang == 'ar' ? 'rtl' : 'ltr'}}">
   <head>
     <!-- Metas -->
     <meta charset="utf-8" />
