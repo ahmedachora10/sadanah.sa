963cf1f3c3bd32b2d8f1376de92caa4e56cec431
ahmedachora10 <gjr8195@gmail.com>
2024-12-13 11:37:51 +0100

    make slider movement smoothly

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -270,7 +270,7 @@ $(function () {
         slidesPerView: 4,
         spaceBetween: 5,
         effect: 'slide',
-        speed: 3000,
+        speed: 5000,
         autoplay: {
             delay: 0,
             disableOnInteraction: false,
