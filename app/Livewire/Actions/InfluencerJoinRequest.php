6fe255da7b56ad48733e8d97bf5b67abfa261b4e
ahmedachora10 <gjr8195@gmail.com>
2024-12-14 13:17:17 +0100

    add influencers join page

--- app/Livewire/Actions/InfluencerJoinRequest.php
+++ app/Livewire/Actions/InfluencerJoinRequest.php
@@ -17,17 +17,8 @@ class InfluencerJoinRequest extends Component
     ];
 
     public function save() {
-        $rules = [];
 
-        foreach ($this->socialMediaPlatforms as $platform) {
-            $rules["form.{$platform}_username"] = 'required|string|max:255';
-            $rules["form.{$platform}_link"] = 'required|url|max:500';
-            $rules["form.{$platform}_followers"] = 'required|integer|min:0';
-        }
-
-        $this->validate($rules);
-
-        $this->form->validate();
+        $this->validate();
 
     }
 
--- app/Livewire/Forms/InfluencerJoinRequestForm.php
+++ app/Livewire/Forms/InfluencerJoinRequestForm.php
@@ -22,6 +22,53 @@ class InfluencerJoinRequestForm extends Form
     #[Validate('required|string')]
     public string $interests = '';
 
+
+    #[Validate('required|string|max:255')]
+    public string $instagram_username = '';
+
+    #[Validate('required|url|max:500')]
+    public string $instagram_link = '';
+
+    #[Validate('required|integer|min:0')]
+    public int $instagram_followers = '';
+
+    #[Validate('required|string|max:255')]
+    public string $tiktok_username = '';
+
+    #[Validate('required|url|max:500')]
+    public string $tiktok_link = '';
+
+    #[Validate('required|integer|min:0')]
+    public int $tiktok_followers = '';
+
+    #[Validate('required|string|max:255')]
+    public string $x_username = '';
+
+    #[Validate('required|url|max:500')]
+    public string $x_link = '';
+
+    #[Validate('required|integer|min:0')]
+    public int $x_followers = '';
+
+    #[Validate('required|string|max:255')]
+    public string $youtube_username = '';
+
+    #[Validate('required|url|max:500')]
+    public string $youtube_link = '';
+
+    #[Validate('required|integer|min:0')]
+    public int $youtube_followers = '';
+
+    #[Validate('required|string|max:255')]
+    public string $snapchat_username = '';
+
+    #[Validate('required|url|max:500')]
+    public string $snapchat_link = '';
+
+    #[Validate('required|integer|min:0')]
+    public int $snapchat_followers = 0;
+
+
     #[Validate('nullable|file')]
     public string $attachments = '';
 }
