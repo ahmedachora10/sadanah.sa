404c5cc4a519dcd0615fde9a64de71180b3b362b
ahmedachora10 <gjr8195@gmail.com>
2024-12-14 11:21:07 +0100

    update influencers

--- resources/views/livewire/container/influencer-join-request.blade.php
+++ resources/views/livewire/container/influencer-join-request.blade.php
@@ -1,35 +1,31 @@
-@include('site.header')
+<x-front-layout>
 
-<!-- ==================== End Navbar ==================== -->
+    <!-- ==================== Start Slider ==================== -->
 
-<div id="smooth-content">
-    <main class="main-bg">
-        <!-- ==================== Start Slider ==================== -->
-
-        <header class="page-header bg-img section-padding" data-background="assets/imgs/header/bg1.jpg"
-            data-overlay-dark="9">
-            <div class="container pt-100">
-                <div class="text-center">
-                    <h1 data-i18n="jobs"></h1>
-                    <div class="mt-15">
-                        <a href="{{route('home')}}">{{__('front.home')}}</a>
-                        <span class="padding-rl-20">|</span>
-                        <span style="color: var(--primary-color)" data-i18n="jobs">{{ trans('front.influencers') }}</span>
-                    </div>
+    <header class="page-header bg-img section-padding" data-background="assets/imgs/header/bg1.jpg" data-overlay-dark="9">
+        <div class="container pt-100">
+            <div class="text-center">
+                <h1 data-i18n="jobs"></h1>
+                <div class="mt-15">
+                    <a href="{{route('home')}}">{{__('front.home')}}</a>
+                    <span class="padding-rl-20">|</span>
+                    <span style="color: var(--primary-color)" data-i18n="jobs">{{ trans('front.influencers')
+                        }}</span>
                 </div>
             </div>
-        </header>
+        </div>
+    </header>
 
-        <!-- ==================== End Slider ==================== -->
+    <!-- ==================== End Slider ==================== -->
 
-        <!-- ==================== Start Blog ==================== -->
+    <!-- ==================== Start Blog ==================== -->
 
-        <section class="blog section-padding">
-            <div class="container">
-                <div class="row xlg-marg">
+    <section class="blog section-padding">
+        <div class="container">
+            <div class="row xlg-marg">
 
-                </div>
             </div>
-        </section>
-    </main>
-    @include('site.footer')
+        </div>
+    </section>
+
+</x-front-layout>
