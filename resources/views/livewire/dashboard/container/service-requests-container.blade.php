1fa9db0b162d81028a6aa795950d648d1fac5e4b
ahmedachora10 <gjr8195@gmail.com>
2024-12-15 22:48:16 +0100

    add service form request

--- resources/views/livewire/dashboard/container/service-requests-container.blade.php
+++ resources/views/livewire/dashboard/container/service-requests-container.blade.php
@@ -44,7 +44,9 @@
                 <p class="text-wrap"><i class="bx bx-category bx-sm me-2"></i>{{ $request?->service?->title }}</p>
                 <p class="text-wrap"><i class="bx bx-user bx-sm me-2"></i>{{ $request?->name }}</p>
                 <p class="text-wrap"><i class="bx bx-phone bx-sm me-2"></i>{{ $request?->phone }}</p>
+                @if($request->attachment != '')
                 <p class="text-wrap"><i class="bx bx-file bx-sm me-2"></i><a href="{{$request?->attachment}}"><i class="bx bx-link-external bx-sm me-2"></i></a></p>
+                @endif
                 <p class="text-wrap">
                     <i class="bx bx-info-square bx-sm me-2"></i>
                     {{ $request?->description }}
