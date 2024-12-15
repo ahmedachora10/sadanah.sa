5e8b15f6b8b09ae591423d684cabdb3b49ff3865
ahmedachora10 <gjr8195@gmail.com>
2024-12-15 22:35:37 +0100

    add service form request

--- app/Models/ServiceRequest.php
+++ app/Models/ServiceRequest.php
@@ -6,10 +6,12 @@ use App\Traits\ThumbnailModelAttribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class ServiceRequest extends Model
+class ServiceRequest extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = ['service_id', 'name', 'phone', 'description'];
 
