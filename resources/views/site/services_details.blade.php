87086a6af886dcf635cb126e3eb895a2db072eb1
ahmedachora10 <gjr8195@gmail.com>
2024-12-15 17:36:51 +0100

    redesign service details page

--- resources/views/site/services_details.blade.php
+++ resources/views/site/services_details.blade.php
@@ -42,16 +42,17 @@
               <div class="sec-head text-center mb-80">
                 <div class="row justify-content-center">
                   <div class="col-lg-8">
-                    <div class="row justify-content-center align-items-center">
+                    <div class="row justify-content-end align-items-center">
                         <div class="col-auto">
                             <img class="serv_details_img" style="width: 90px !important" src="{{asset('storage/'.$service->image)}}" alt="service image" data-speed="auto" data-lag="0" />
                         </div>
-                      <div class="col-md-8 col-9 text-end mb-5">
+                      <div class="col-md-8 col-9 text-end">
                         <h4>
                             {{$title}}
                         </h4>
                       </div>
-                      <div class="col-12 mt-5">
+                      <div class="col-12" style="margin: 3rem 0"></div>
+                      <div class="col-12">
                         <div class="text md-mb50 ">
                             <p class="fs-5 text-end">
                                 {{$description}}
