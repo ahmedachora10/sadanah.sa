7f3c171d21725a851342d4f259c5aea2ebdd1228
ahmedachora10 <gjr8195@gmail.com>
2024-12-15 22:41:47 +0100

    add service form request

--- resources/views/livewire/store-service-request.blade.php
+++ resources/views/livewire/store-service-request.blade.php
@@ -1,5 +1,7 @@
 <form method="POST" wire:submit="save" class="row" style="flex-direction: row !important">
-
+    <div class="col-12">
+        <livewire.dashboard.alert />
+    </div>
     <div class="col-md-4 col-sm-6 col-12 mb-3">
         <x-dashboard.input-group type="text" wire:model.defer="form.name" name="form.name" title="" placeholder="{{trans('table.columns.name')}}(*)" />
     </div>
