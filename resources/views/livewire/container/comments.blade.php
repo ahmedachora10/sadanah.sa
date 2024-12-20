175a4d8ea25df08e37d9a81a5d075ed7674b9873
ahmedachora10 <gjr8195@gmail.com>
2024-12-20 18:05:28 +0100

    update

--- resources/views/livewire/container/comments.blade.php
+++ resources/views/livewire/container/comments.blade.php
@@ -40,44 +40,7 @@
     </x-dashboard.tables.table1>
 
     <div class="mt-4">
-        {{ $data->links() }}
+        {{ $comments->links() }}
     </div>
 
-    <x-dashboard.modals.modal1 id="showMessage">
-        <div class="col-12">
-            <ul class="list-unstyled my-3 py-1">
-                @foreach ($request?->getFillable() ?? [] as $item)
-                @if(in_array($item, ['instagram','snapchat','tiktok','youtube','x']))
-                <li class="mt-3 mb-2">
-                    <h5 class="text-muted fw-bold">{{ trans('front.'.$item) }}</h5>
-                </li>
-                @foreach ($request->$item ?? [] as $key => $inf)
-                <li class="d-flex align-items-center mb-4">
-                    <i class="bx bx-check"></i>
-                    <span class="fw-medium mx-2">{{trans('table.columns.'.$key)}}:</span>
-                    <span>@if($key == 'link') <a class="text-warning" href="{{$inf}}" target="_blank"><i
-                                class="bx bx-link-external"></i></a> @else {{$inf}} @endif</span>
-                </li>
-                @endforeach
-                @else
-                <li class="d-flex align-items-center mb-4">
-                    <i class="bx bx-check"></i>
-                    <span class="fw-medium mx-2">{{trans('table.columns.'.str($item)->replace('_', '
-                        ')->value())}}:</span>
-                    <span>{{$request?->$item}}</span>
-                </li>
-                @endif
-                @endforeach
-                @if($request?->thumbnail != '')
-                <li class="d-flex align-items-center mb-4">
-                    <i class="bx bx-check"></i>
-                    <span class="fw-medium mx-2">{{trans('table.columns.attachments')}}:</span>
-                    <a href="{{$request?->thumbnail}}" target="_blank">
-                        <i class="bx bx-file text-primary"></i>
-                    </a>
-                </li>
-                @endif
-            </ul>
-        </div>
-    </x-dashboard.modals.modal1>
 </section>
