5929973afad3af5b6818ab8a8e8974fdf8750886
ahmedachora10 <gjr8195@gmail.com>
2024-12-20 15:17:40 +0100

    update blog page details

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Blog extends Model
 {
-    use HasFactory , ThumbnailModelAttribute, HasTranslation;
+    use HasFactory , ThumbnailModelAttribute;
 
     protected $fillable = [
         'title_ar',
@@ -24,6 +24,13 @@ class Blog extends Model
         parent::boot();
 
         static::retrieved(fn(Blog $blog) => static::translation($blog));
+    }
+
+    public function getTitleAttribute() {
+        return app()->getLocale() === 'ar' ? $this->title_ar : $this->title_en;
+    }
+    public function getContentAttribute() {
+        return app()->getLocale() === 'ar' ? $this->content_ar : $this->content_en;
     }
 
     public function comments(): HasMany {
