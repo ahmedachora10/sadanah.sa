6556c7f385aff8c6936252c949a32826ebbca712
ahmedachora10 <gjr8195@gmail.com>
2024-12-21 17:48:16 +0100

    udpate blogs

--- resources/views/site/blog_details.blade.php
+++ resources/views/site/blog_details.blade.php
@@ -54,8 +54,8 @@
                       </article>
                     </div>
                     <div class="d-flex justify-content-end gap-2">
-                        <span class="text-muted">{{ $blog->job_title }}</span>
-                        <span class="text-muted">{{ $blog->created_at->format('Y F d') }}</span>
+                        <span class="text-muted fs-6"><i class="fas fa-user mx-1"></i>{{ $blog->job_title }}</span>
+                        <span class="text-muted fs-6"><i class="fas fa-calender mx-1"></i>{{ $blog->created_at->format('Y F d') }}</span>
                     </div>
                     <div class="info-area flex pt-50 bord-thin-top">
                       <div class="ml-auto">
