6aee41605339cf9ac745be3829d50c38b189d3b5
ahmedachora10 <gjr8195@gmail.com>
2024-12-22 18:24:31 +0100

    update instagram feed profile name

--- app/Services/InstagramService.php
+++ app/Services/InstagramService.php
@@ -5,7 +5,7 @@ namespace App\Services;
 use Dymantic\InstagramFeed\InstagramFeed;
 
 class InstagramService {
-    const PROFILE_NAME = 'dashan';
+    const PROFILE_NAME = 'sadnah';
 
     public static function auth() {
         return \Dymantic\InstagramFeed\Profile::new(self::PROFILE_NAME)->getInstagramAuthUrl();
