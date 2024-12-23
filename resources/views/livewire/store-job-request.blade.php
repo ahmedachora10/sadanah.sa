55d1412e5ac0847452a1cbedb184950227615fc0
ahmedachora10 <gjr8195@gmail.com>
2024-12-23 05:27:04 +0100

    add job request

--- resources/views/livewire/store-job-request.blade.php
+++ resources/views/livewire/store-job-request.blade.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row text-end">
 
     @if (session('success'))
         <div class="col-12 mb-3">
@@ -7,40 +7,40 @@
     @endif
 
     <div class="col-xl-6 col-lg-6 mb-3">
-        <x-dashboard.input-group type="text" wire:model.defer="form.name" name="form.name" :placeholder="trans('table.columns.name')" />
+        <x-dashboard.input-group type="text" wire:model.defer="form.name" name="form.name" title="" :placeholder="trans('table.columns.name')" />
     </div>
 
     <div class="col-xl-6 col-lg-6 mb-3">
-        <x-dashboard.input-group type="text" wire:model.defer="form.email" name="form.email" :placeholder="trans('table.columns.email')" />
+        <x-dashboard.input-group type="text" wire:model.defer="form.email" name="form.email" title="" :placeholder="trans('table.columns.email')" />
     </div>
 
     <div class="col-xl-6 col-lg-6 mb-3">
-        <x-dashboard.input-group type="number" wire:model.defer="form.phone" name="form.phone" :placeholder="trans('table.columns.phone')" />
+        <x-dashboard.input-group type="number" wire:model.defer="form.phone" name="form.phone" title="" :placeholder="trans('table.columns.phone')" />
     </div>
 
     <div class="col-xl-6 col-lg-6 mb-3">
-        <x-dashboard.input-group type="number" wire:model.defer="form.age" name="form.age" :placeholder="trans('table.columns.age')" />
+        <x-dashboard.input-group type="number" wire:model.defer="form.age" name="form.age" title="" :placeholder="trans('table.columns.age')" />
     </div>
 
     <div class="col-xl-6 col-lg-6 mb-3">
         <x-dashboard.input-group type="text" wire:model.defer="form.specialization" name="form.specialization"
-            :placeholder="trans('table.columns.specialization')" />
+            title="" :placeholder="trans('table.columns.specialization')" />
     </div>
 
     <div class="col-xl-6 col-lg-6 mb-3">
         <x-dashboard.input-group type="text" wire:model.defer="form.years_of_experience"
-            name="form.years_of_experience" :placeholder="trans('table.columns.years of experience')" />
+            name="form.years_of_experience" title="" :placeholder="trans('table.columns.years of experience')" />
     </div>
 
     <div class="col-12 mb-3">
-        <x-dashboard.text-area wire:model.defer="form.excerpt" :placeholder="trans('table.columns.excerpt')" name="form.excerpt" cols="10" rows="6">
+        <x-dashboard.text-area wire:model.defer="form.excerpt" title="" :placeholder="trans('table.columns.excerpt')" name="form.excerpt" cols="10" rows="6">
             <x-dashboard.error field="form.excerpt" />
         </x-dashboard.text-area>
     </div>
 
     <div class="col-xl-6 col-lg-6 mb-3">
         <div class="country-select">
-            <select name="job" id="job" wire:model.defer="form.job">
+            <select class="form-select" name="job" id="job" wire:model.defer="form.job">
                 <option value=""><x-dashboard.label>{{ trans('table.columns.job') }}</x-dashboard.label></option>
                 @foreach ($jobs as $job)
                     <option value="{{ $job->id }}">{{ $job->title }}</option>
@@ -52,7 +52,7 @@
 
     <div class="col-xl-6 col-lg-6 mb-3">
         <div class="country-select">
-            <select name="job_city" id="job_city" wire:model.defer="form.job_city">
+            <select class="form-select" name="job_city" id="job_city" wire:model.defer="form.job_city">
                 <option value=""><x-dashboard.label>{{ trans('table.columns.job city') }}</x-dashboard.label></option>
                 @foreach ($jobCities as $city)
                     <option value="{{ $city->id }}">{{ $city->name }}</option>
@@ -72,6 +72,6 @@
     </div>
 
     <div class="col-12">
-        <button class="send_button" type="button" wire:click="save">{{ trans('send') }}</button>
+        <button class="confirm_button" type="button" wire:click="save">{{ trans('send') }}</button>
     </div>
 </div>
