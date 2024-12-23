a09dcbc9e4e00f0299e28367ec5bbb76008ea60a
ahmedachora10 <gjr8195@gmail.com>
2024-12-23 16:48:02 +0100

    update job request page

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,17 +3,17 @@
     <main class="main-bg">
         <!-- ==================== Start Header ==================== -->
 
-        <header class="page-header-cerv bg-img section-padding" data-background="{{asset('assets/imgs/header/2.jpg')}}"
+        <header class="page-header-cerv bg-img section-padding" data-background="{{asset('assets/imgs/header/job-posting-hiring.jpg')}}"
             data-overlay-dark="4">
             <div class="container pt-100 ontop">
                 <div class="text-center">
-                    <h1 class="fz-100" data-i18n="Services">{{__('front.services')}}</h1>
+                    <h1 class="fz-100" data-i18n="Services">{{__('front.jobs')}}</h1>
                     <div class="mt-15">
                         <a href="{{route('home')}}" data-i18n="home">{{__('front.home')}}</a>
                         <span class="padding-rl-20">|</span>
-                        <a href="{{route('home')}}" data-i18n="jobs">{{__('front.jobs')}}</a>
+                        <a href="{{route('job')}}" data-i18n="jobs">{{__('front.jobs')}}</a>
                         <span class="padding-rl-20">|</span>
-                        <span style="color: var(--primary-color)" data-i18n="job-title">{{$job->title}}</span>
+                        <span style="color: var(--primary-color)" data-i18n="job-title">{{$job->{'title_'.app()->getLocale()}}}</span>
                     </div>
                 </div>
             </div>
