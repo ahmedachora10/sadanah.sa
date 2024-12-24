544e941cb1bd17355289da48c1c67c074e61b4ac
ahmedachora10 <gjr8195@gmail.com>
2024-12-24 16:23:47 +0100

    update instagram service

--- config/setting_fields.php
+++ config/setting_fields.php
@@ -57,7 +57,7 @@ return [
                 'data' => 'int', // data type, string, int, boolean
                 'name' => 'instagram_posts_count', // unique name for field
                 'label' => 'Instagram Posts Count', // you know what label it is
-                'rules' => 'nullable|min:2', // validation rule of laravel
+                'rules' => 'nullable|integer', // validation rule of laravel
                 'class' => '', // any class for input
                 'value' => 6 // default value if you want
             ],
