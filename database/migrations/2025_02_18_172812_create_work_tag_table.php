18e69f27b13ee4d69096337441488b149cef24c6
ahmedachora10 <gjr8195@gmail.com>
2025-02-18 15:46:29 +0100

    add multiple tags option

--- database/migrations/2025_02_18_172812_create_work_tag_table.php
+++ database/migrations/2025_02_18_172812_create_work_tag_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('work_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(OurWork::class);
-            $table->foreignId(Tag::class);
+            $table->foreignIdFor(Tag::class);
             $table->timestamps();
         });
     }
